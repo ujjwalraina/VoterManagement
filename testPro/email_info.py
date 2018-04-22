93c27d334c26f467b5cc75e3d84e7bc341176637
Ujjwal Raina <ujjwal.1rn13cs109@gmail.com>
2018-04-22 14:52:00 +0530

    Update email_info.py

--- testPro/email_info.py
+++ testPro/email_info.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ujjwal07raina@gmail.com'
-EMAIL_HOST_PASSWORD = 'ujj17121995'
+EMAIL_HOST_USER = 'myEmail@mail.com'
+EMAIL_HOST_PASSWORD = 'myPassword'
 EMAIL_PORT = 587
-EMAIL_TO_LIST = 'ujjwal_raina@outlook.com'
+EMAIL_TO_LIST = 'To_list'
